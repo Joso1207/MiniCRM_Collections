e9cef687bd1ab42621bde3a7c3eb59cd0d844444
Joso1207 <joso1207@student.miun.se>
2025-09-30 08:09:40 +0200

    Init + base classes

--- src/main/java/org/example/CRMApp.java
+++ src/main/java/org/example/CRMApp.java
@@ -3,10 +3,12 @@ package org.example;
 
 import java.util.*;
 
-public class CrmApp {
-    private final Map<String,Customer>;
+public class CRMApp {
+    private final Map<String,Customer> Storage;
     private final Scanner scanner = new Scanner(System.in);
 
 
-
+    public CRMApp(Map<String, Customer> storage) {
+        Storage = storage;
+    }
 }
