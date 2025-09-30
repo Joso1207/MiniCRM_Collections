58f88c29a58e45b755507c1a823338d8966f018e
Joso1207 <joso1207@student.miun.se>
2025-09-30 08:51:49 +0200

    Customer class implemented

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -5,10 +5,87 @@ import java.util.*;
 public class Customer {
 
     private final String id;
-    private String name;
+    private static Integer lastId = 0;
+
+    private final String name;
     private final Set<String> emails = new LinkedHashSet<>();
     private final Set<String> tags = new LinkedHashSet<>();
     private final List<String> notes = new ArrayList<>();
 
 
+    public Customer(String name) {
+        this.name = name;
+        lastId++;
+        this.id = lastId.toString();
+    }
+
+    public void addEmail(String email){
+        emails.add(email);
+    }
+    public void listEmail(){
+        for(String s : emails){
+            System.out.println(s);
+        }
+    }
+
+    public void removeEmail(String email){
+        emails.remove(email);
+    }
+
+    public void addTag(String tag){
+        tags.add(tag);
+    }
+
+    public void listTags(){
+        for(String s : tags){
+            System.out.println(s);
+        }
+    }
+
+    public void removeTag(String tag){
+        tags.remove(tag);
+    }
+
+    public void addNote(String note){
+        notes.add(note);
+    }
+
+    public String getNote(int index){
+        return notes.get(index);
+    }
+
+    public void listNotes(){
+        for (String s : notes){
+            System.out.println(s);
+        }
+    }
+
+    public void removeNote(int index){
+        notes.remove(index);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Set<String> getEmails() {
+        return emails;
+    }
+
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public List<String> getNotes() {
+        return notes;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{id='"+ id + "', name='"+name+ "',tags='"+tags+"'}";
+    }
 }
